6dac74596883219df73831d5666ae0923cacff46
MUTALE GEORGE <148863672+GEORGEMUTALE@users.noreply.github.com>
2024-04-05 23:28:00 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {year} is a leap year")
+    (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0 )
+}
+
+ fn main() {
+    println!("Enter a year to check if it  is a leap");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let year: u64 = input.trim().parse().expect("Please enter a valid year");
+
+    if is_leap_year(year) {
+        println!("{} is a leap year", year)
+    } else{
+        println!("{} is not a leap", year)
+    }
 }
